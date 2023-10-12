7e8f315d1a012794fe479e41ab4e41bc09b6c53b
Denny Doan <denny-333@hotmail.com>
2023-10-12 16:29:12 -0400

    Added Player seed file to db

---
+++ server/db/seed/Players.ts
@@ -0,0 +1,18 @@
+// src/seed/playersSeed.ts
+import { PlayerModel } from '../models/PlayersModel';
+
+// Function to seed Player data
+const playersSeed = async () => {
+  try {
+    await PlayerModel.create({
+      roomId: 'your_game_room_id',
+      playerName: 'Player 1',
+      team: 'Team A',
+    });
+    // Add more players as needed
+  } catch (error) {
+    console.error('Error seeding players:', error);
+  }
+};
+
+export default playersSeed;
