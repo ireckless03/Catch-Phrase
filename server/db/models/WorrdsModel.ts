2be9bc989a76faeb1924651c2aad5f44e78f6aac
Denny Doan <denny-333@hotmail.com>
2023-10-12 16:30:40 -0400

    Added word models to db

---
+++ server/db/models/WorrdsModel.ts
@@ -0,0 +1,24 @@
+// src/models/WordsModel.ts
+import { Schema, Document, model } from 'mongoose';
+
+// Define the shape of a GameWord document
+export interface GameWord {
+  word: string;
+  team: string;
+  guessed: boolean;
+  skip: boolean;
+}
+
+// Define the GameWord document type that extends Mongoose's Document
+export interface GameWordDocument extends Document, GameWord {}
+
+// Create a Mongoose schema for the GameWord collection
+const gameWordSchema = new Schema<GameWordDocument>({
+  word: { type: String, required: true },
+  team: { type: String, required: true },
+  guessed: { type: Boolean, default: false },
+  skip: { type: Boolean, default: false },
+});
+
+// Create a Mongoose model for the GameWord collection
+export const GameWordModel = model<GameWordDocument>('GameWord', gameWordSchema);
